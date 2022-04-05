e06df226f3d731aab79d60c8e43e99f884b91fa9
marcdevrenan <marcdevrenan@gmail.com>
2022-04-05 08:15:59 -0300

    added about

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,11 @@ import React from "react";
 export default function About() {
     return (
         <div>
-            <h2>TODO</h2>
+            <h1>Instituto Infnet</h1>
+            <h2>AT - Frameworks Front-End e Conexão com Back-End</h2>
+            <h3>Prof(a): Vanessa Cristina</h3>
+            <h3>Aluno: Renan Ferreira Marcílio</h3>
+            <p>Projeto de listagem de tarefas feito com React para entrega final da disciplina</p>
         </div>
     )
 }
